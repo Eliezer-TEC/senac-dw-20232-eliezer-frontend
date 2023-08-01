d6d533974f66802a1cc8ee88f7e50100f99ec5dd
Eliezer Silva <eliezerliz2704@gmail.com>
2023-08-01 10:46:25 -0300

    Buscador de CEP

--- js/exemplo.js
+++ js/exemplo.js
@@ -1,3 +1,6 @@
+// npm install prompt-sync
+
+
 
 // -----Entrada e Saida------
 
@@ -11,30 +14,55 @@
 // -----Entrada e Saida------
 
 // -----Repetição------
-let carroA = { fabricante: 'Ford', modelo: 'Mustang', ano: 1969 };
-let carroB = { fabricante: 'Porsche', modelo: 'Macan', ano: 2023 };
-let carros = [carroA, carroB];
+// let carroA = { fabricante: 'Ford', modelo: 'Mustang', ano: 1969 };
+// let carroB = { fabricante: 'Porsche', modelo: 'Macan', ano: 2023 };
+// let carros = [carroA, carroB];
 
-for (let carro of carros) {
-    console.log(carro);
-}
-console.log('----------------------');
-for (let propriedade in carroA) {
-    console.log(propriedade);
-}
-console.log('----------------------');
-for (let propriedade in carroB) {
-    console.log('Propriedade: ' + propriedade
-        + ' - valor: ' + carroB[propriedade]);
-}
-console.log('----------------------');
-let nomes = ['Fred', 'Sheila', 'Sara '];
-for (let nome of nomes) {
-    console.log(nome);
-}
-console.log('----------------------');
-for (let nome in nomes) {
-    console.log(nome);
-}
+// for (let carro of carros) {
+//     console.log(carro);
+// }
+// console.log('----------------------');
+// for (let propriedade in carroA) {
+//     console.log(propriedade);
+// }
+// console.log('----------------------');
+// for (let propriedade in carroB) {
+//     console.log('Propriedade: ' + propriedade
+//         + ' - valor: ' + carroB[propriedade]);
+// }
+// console.log('----------------------');
+// let nomes = ['Fred', 'Sheila', 'Sara '];
+// for (let nome of nomes) {
+//     console.log(nome);
+// }
+// console.log('----------------------');
+// for (let nome in nomes) {
+//     console.log(nome);
+// }
 // -----Repetição------
+
+// -----BuscarCEP------
+
+// instanciando o prompt 
+let prompt = require('prompt-sync')();
+
+// - Solicitando o CEP
+let cepInf = prompt('Qual o seu cep?');
+
+// Chamar a função para consultar o CEP
+buscarCEP(cepInf);
+async function buscarCEP(cep) {
+
+    let options = {
+        method: "GET",
+        headers: { "Content-type": "application/json" }
+    };
+
+    const promiseConsultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`, options);
+
+    const json = await promiseConsultaCEP.json();
+
+    console.log(json);
+}
+
 
