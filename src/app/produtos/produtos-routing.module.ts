960c617907261072636d39be10989e47a66d84ae
Eliezer Silva <eliezerliz2704@gmail.com>
2023-09-28 10:55:16 -0300

    Criação dos modulos e componentes de produto

---
+++ src/app/produtos/produtos-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule, OnInit } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProdutoListagemComponent } from './produto-listagem/produto-listagem.component';
+
+const routes: Routes = [
+  {path: 'lista', component: ProdutoListagemComponent}
+];
+
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ProdutosRoutingModule implements OnInit {
+
+  constructor(){
+
+  }
+  ngOnInit(): void {
+
+  }
+}
